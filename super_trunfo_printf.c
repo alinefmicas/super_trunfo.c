aaa21c7ae3f358c3a4693c1788fae669b4a41232
Aline Franco Micas <aline_micas@hotmail.com>
2025-09-07 20:15:31 +0000

    cartas_supertrunfo

---
+++ super_trunfo_printf.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int main(){
+    printf("SUPER TRUNFO\n\n");
+    printf("CARTA 01\n");
+    printf("Estado: Bahia\n");
+    printf("Código: B01\n");
+    printf("Capital: Salvador\n");
+    printf("População: 2.564.204 habitantes\n");
+    printf("Área: 693,442km²\n");
+    printf("PIB: 62.954 bilhões\n");
+    printf("Pontos Turisticos: 78\n\n");
+
+    printf("CARTA 02\n");
+    printf("Estado: Paraná\n");
+    printf("Código: P02\n");
+    printf("Capital: Curitiba\n");
+    printf("População: 1.830.795 habitantes\n");
+    printf("Área: 432km²\n");
+    printf("PIB: 62.954 bilhões\n");
+    printf("Pontos Turisticos: 32\n");
+    
+
+
+
+
+    return 0;
+}
