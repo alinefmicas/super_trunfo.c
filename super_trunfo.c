9dca2cf95c247d507a3c5b5615f76b3ee1d1f7b1
Aline Franco Micas <aline_micas@hotmail.com>
2025-09-07 18:55:38 +0000

    Estrutura da carta

---
+++ super_trunfo.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+    char Estado[50];
+    char Código[5];
+    char Capital[50];
+    int População;
+    float Área;
+    float PIB;
+    int Pontos_Turisticos;
+    
+}
